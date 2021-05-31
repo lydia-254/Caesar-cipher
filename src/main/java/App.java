ba35c3b8bab8fc37a8d653dd0c894aaeb7ff92a2
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-05-31 14:28:57 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,14 @@ public class App {
             System.out.println(Select "e" for encryption or "d" for decryption);
             Scanner input= new Scanner(Scanner.in);
             String choice = input.nextLine().toLowerCase();
+            if (choice.equals("e")){
+                System.out.println("Input The Text For Encryption");
+                String plainText = input.nextLine().toLowerCase();
+                System.out.println("Input The Random Value");
+                int none = input.nextInt();
+                App message = new App(plainText, none);
+
+            }
         }
     }
 }
