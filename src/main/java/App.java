e7536b13547485691aca3291f329f2bc6b5b60d5
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-05-31 19:58:27 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,10 @@
 public class App {
-    public static void main(String[] args) {
-        System.out.println(Hello,Welcome to my Caesar-cipher Website.);
-        while (true){
-            System.out.println(Select "e" for encryption or "d" for decryption);
-            Scanner input= new Scanner(Scanner.in);
-            String choice = input.nextLine().toLowerCase();
-            if (choice.equals("e")){
-                System.out.println("Input The Test For Encryption");
-                String planeText = input.nextLine().toLowerCase();
-                System.out.println("Input The Random Value");
-                int num = input.nextInt();
-                App message = new App(planeText, num);
-                System.out.println("plane Text:" + message.getMessage() + "/n new value of:" + message.getNum());
-                System.out.println("Encrypt Test:" + message.encryption());
-                break;
+    public String message;
+    public int num;
+    public  App(String message, int num){
+        this.message = message;
 
-            }
-            else if (choice.equals("d")){
-                System.out.println("Input Test to Decrypt");
-                String encryptedText = input.nextLine().toLowerCase();
-                System.out.println("Enter numValue");
-                int reversValue = input.nextInt();
-                App encryptText = new App(encryptedText, reversValue);
-                System.out.println("decrypted message:" + encryptText.decryption());
-                break;
-            }
-            else if {
-                System.out.println("invalid choice");
-            }
-        }
     }
 }
+
+
--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
 public class Caesar {
+    public static void main(String[] args) {
+        System.out.println(Hello,Welcome to my Caesar-cipher Website.);
+        while (true){
+            System.out.println(Select "e" for encryption or "d" for decryption);
+            Scanner input= new Scanner(Scanner.in);
+            String choice = input.nextLine().toLowerCase();
+            if (choice.equals("e")){
+                System.out.println("Input The Test For Encryption");
+                String planeText = input.nextLine().toLowerCase();
+                System.out.println("Input The Random Value");
+                int num = input.nextInt();
+                App message = new App(planeText, num);
+                System.out.println("plane Text:" + message.getMessage() + "/n new value of:" + message.getNum());
+                System.out.println("Encrypt Test:" + message.encryption());
+                break;
+
+            }
+            else if (choice.equals("d")){
+                System.out.println("Input Test to Decrypt");
+                String encryptedText = input.nextLine().toLowerCase();
+                System.out.println("Enter numValue");
+                int reversValue = input.nextInt();
+                App encryptText = new App(encryptedText, reversValue);
+                System.out.println("decrypted message:" + encryptText.decryption());
+                break;
+            }
+            else if {
+                System.out.println("invalid choice");
+            }
+        }
+    }
 }
+
