4d943ed768880d06a58dbf2b47e7470c1cd1c4c6
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-05-31 14:15:14 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,10 @@
 public class App {
+    public static void main(String[] args) {
+        System.out.println(Hello,Welcome to my Caesar-cipher Website.);
+        while (true){
+            System.out.println(Select "e" for encryption or "d" for decryption);
+            Scanner input= new Scanner(Scanner.in);
+            String choice = input.nextLine().toLowerCase();
+        }
+    }
 }
